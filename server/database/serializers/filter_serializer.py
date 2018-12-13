1b2d3c5129951e71099fdf4029baa47c80ad7c69
Abhinand Sivaprasad <abhi@live.com>
2018-12-12 23:14:25 -0500

    fixed serialization bug

--- server/database/serializers/filter_serializer.py
+++ server/database/serializers/filter_serializer.py
@@ -1,11 +1,11 @@
 class FilterSerializer:
     def serialize(self, filter):
         return {
+            'filterId': filter.id,
             'firstName': filter.first_name,
             'lastName': filter.last_name,
             'classYear': filter.year,
             'college': filter.college,
             'major': filter.major,
-            'birthday': filter.birth_day,
-            'room': filter.room
+            'birthday': filter.birth_day
         }
